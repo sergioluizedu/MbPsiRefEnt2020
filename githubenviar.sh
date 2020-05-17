46ab6246f1af75412b752475b6f3cd3d08cf78c5
Sérgio Luiz Eduardo <65469090+sergioluizedu@users.noreply.github.com>
2020-05-17 15:34:59 -0300

    Delete githubenviar.sh

--- githubenviar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git init
-git config --global user.name"sergioluizedu"
-git config --global user.email"sergioluizufrn@ufrn.edu.br"
-git init
-git remote add origin https://github.com/sergioluizedu/MbPsiRefEnt2020.git
-#git add .
-#git commit -m "version 0"
-#git push origin master
